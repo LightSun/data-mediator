fa263f0a16adfcd3a4458740b68f0460863aa418
LightSun <532278976@qq.com>
2017-08-29 18:51:24 +0800

    add  demo

--- Data-mediator-demo/app/src/main/java/com/heaven7/data/mediator/demo/StudentBind.java
+++ Data-mediator-demo/app/src/main/java/com/heaven7/data/mediator/demo/StudentBind.java
@@ -8,6 +8,7 @@ import com.heaven7.java.data.mediator.ICacheable;
 import com.heaven7.java.data.mediator.ICopyable;
 
 import java.text.Format;
+import static com.heaven7.java.data.mediator.processor.FieldData.*;
 
 /**
  * Created by Administrator on 2017/8/28 0028.
@@ -17,7 +18,8 @@ import java.text.Format;
      @Field(propName = "test_object", seriaName = "test_object",flags = 1,type = Object.class),
      @Field(propName = "test_Format", seriaName = "test_Format",flags = 1,type = Format.class),
      @Field(propName = "test_int", seriaName = "test_int",flags = 1,type = int.class),
-     @Field(propName = "test_double", seriaName = "test_double",flags = 1,type = double.class)
+     @Field(propName = "test_list", seriaName = "test_list",flags = 1,type = long.class, complexType = COMPLEXT_LIST),
+     @Field(propName = "test_array", seriaName = "test_array",flags = 1,type = String.class, complexType = COMPLEXT_ARRAY),
 })
 @TargetApi(14)
 public interface StudentBind extends ICopyable, ICacheable{
---
+++ Data-mediator-demo/app/src/main/java/com/heaven7/data/mediator/demo/TestInterface2.java
@@ -0,0 +1,8 @@
+package com.heaven7.data.mediator.demo;
+
+/**
+ * Created by Administrator on 2017/8/29 0029.
+ */
+
+public interface TestInterface2 extends StudentBind {
+}
