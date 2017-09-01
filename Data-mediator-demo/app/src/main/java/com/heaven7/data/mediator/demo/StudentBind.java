0fd2c48326fa9dcd609deaf9b5a0861cc2e70ac0
LightSun <532278976@qq.com>
2017-09-01 19:52:29 +0800

    change

--- Data-mediator-demo/app/src/main/java/com/heaven7/data/mediator/demo/StudentBind.java
+++ Data-mediator-demo/app/src/main/java/com/heaven7/data/mediator/demo/StudentBind.java
@@ -8,6 +8,8 @@ import java.text.Format;
 
 import static com.heaven7.java.data.mediator.processor.FieldData.COMPLEXT_ARRAY;
 import static com.heaven7.java.data.mediator.processor.FieldData.COMPLEXT_LIST;
+import static com.heaven7.java.data.mediator.processor.FieldData.FLAG_EXPOSE_DEFAULT;
+import static com.heaven7.java.data.mediator.processor.FieldData.FLAG_EXPOSE_SERIALIZE_FALSE;
 import static com.heaven7.java.data.mediator.processor.FieldData.FLAG_SERIALIZABLE;
 
 /**
@@ -15,9 +17,10 @@ import static com.heaven7.java.data.mediator.processor.FieldData.FLAG_SERIALIZAB
  */
 @Fields({
      @Field(propName = "name", seriaName = "heaven7",flags = FLAG_SERIALIZABLE,type = String.class),
-     @Field(propName = "test_object", seriaName = "test_object",flags = 1,type = Object.class),
+     @Field(propName = "test_object", seriaName = "test_object",
+             flags = FLAG_EXPOSE_DEFAULT|FLAG_EXPOSE_SERIALIZE_FALSE,type = Object.class),
      @Field(propName = "test_Format", seriaName = "test_Format",flags = 1,type = Format.class),
-     @Field(propName = "test_int", seriaName = "test_int",flags = 1,type = int.class),
+     @Field(propName = "test_int", seriaName = "test_int",flags = FLAG_EXPOSE_DEFAULT,type = int.class),
      @Field(propName = "test_list", seriaName = "test_list",flags = 1,type = long.class, complexType = COMPLEXT_LIST),
      @Field(propName = "test_array", seriaName = "test_array",flags = 1,type = String.class, complexType = COMPLEXT_ARRAY),
 })
