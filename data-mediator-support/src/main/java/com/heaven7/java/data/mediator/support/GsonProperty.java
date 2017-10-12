0e7e843d1c0a96e6227bf44e405adb985f3fd0f1
LightSun <532278976@qq.com>
2017-10-12 13:57:51 +0800

    add doc

--- data-mediator-support/src/main/java/com/heaven7/java/data/mediator/support/GsonProperty.java
+++ data-mediator-support/src/main/java/com/heaven7/java/data/mediator/support/GsonProperty.java
@@ -1,7 +1,28 @@
+/**
+ * Copyright 2017
+ group of data-mediator
+ member: heaven7(donshine723@gmail.com)
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 package com.heaven7.java.data.mediator.support;
 
 import com.heaven7.java.data.mediator.Property;
 
+/**
+ * the gson property which is an extend class of {@linkplain Property}
+ * @author heaven7
+ */
 public class GsonProperty extends Property {
 
     private String seriaName;
