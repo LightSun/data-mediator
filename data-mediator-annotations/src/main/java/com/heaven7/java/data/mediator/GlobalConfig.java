44a9c2f147231c5a2812f2a827b61325a9dcfffb
LightSun <532278976@qq.com>
2017-10-13 10:19:19 +0800

    update doc

--- data-mediator-annotations/src/main/java/com/heaven7/java/data/mediator/GlobalConfig.java
+++ data-mediator-annotations/src/main/java/com/heaven7/java/data/mediator/GlobalConfig.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * the global setting for data-mediator.
+ * the global gson config for data-mediator.
  * @since 1.0.5
  * @author heaven7
  */
@@ -15,19 +15,8 @@ import java.lang.annotation.Target;
 public @interface GlobalConfig {
 
     /**
-     * the current version for gson serialize/deserialize json.
-     * <p>this will cause create gson something like this: </p>
-     * <code><pre>
-     *      Gson gson = new GsonBuilder().setVersion(2.0).create();
-     * </pre></code>
-     * @return the current version of gson,
+     * the gson configof 'Google/Gson'
+     * @return the gson config.
      */
-    double gsonVersion() default 1.0;
-
-    /**
-     * use {@literal @}JsonAdapter or not. that means is this is enabled .
-     * it will auto generate TypeAdapter and generate {@literal @}JsonAdapter.
-     * @return true if enable json adapter or not. you can see more in 'Google/Gson'.
-     */
-    boolean jsonAdapter() default true;
+    GsonConfig gsonConfig() default @GsonConfig();
 }
---
+++ data-mediator-annotations/src/main/java/com/heaven7/java/data/mediator/GsonConfig.java
@@ -0,0 +1,33 @@
+package com.heaven7.java.data.mediator;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * the global setting for data-mediator.
+ * @since 1.0.5
+ * @author heaven7
+ */
+@Target(ElementType.ANNOTATION_TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface GsonConfig {
+
+    /**
+     * the current version for gson serialize/deserialize json.
+     * <p>this will cause create gson something like this: </p>
+     * <code><pre>
+     *      Gson gson = new GsonBuilder().setVersion(2.0).create();
+     * </pre></code>
+     * @return the current version of gson,
+     */
+    double version() default 1.0;
+
+    /**
+     * use {@literal @}JsonAdapter or not. that means is this is enabled .
+     * it will auto generate TypeAdapter and generate {@literal @}JsonAdapter.
+     * @return true if enable json adapter or not. you can see more in 'Google/Gson'.
+     */
+    boolean generateJsonAdapter() default true;
+}
