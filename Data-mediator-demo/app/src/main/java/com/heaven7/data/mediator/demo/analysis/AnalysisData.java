3bec02cbbdb7c5c5d012d9fc7b5d5dced2012a85
LinghtSun <532278976@qq.com>
2017-10-25 07:45:16 +0800

    fix plugin bug of id name

--- Data-mediator-demo/app/src/main/java/com/heaven7/data/mediator/demo/analysis/AnalysisData.java
+++ Data-mediator-demo/app/src/main/java/com/heaven7/data/mediator/demo/analysis/AnalysisData.java
@@ -1,10 +1,13 @@
 package com.heaven7.data.mediator.demo.analysis;
 
 import com.heaven7.data.mediator.demo.module.FlowItem;
+import com.heaven7.java.data.mediator.DataPools;
 import com.heaven7.java.data.mediator.Field;
 import com.heaven7.java.data.mediator.Fields;
 import com.heaven7.java.data.mediator.ICopyable;
 import com.heaven7.java.data.mediator.IResetable;
+import com.heaven7.java.data.mediator.Property;
+import com.heaven7.java.data.mediator.internal.SharedProperties;
 
 import static com.heaven7.java.data.mediator.FieldFlags.*;
 /**
@@ -24,5 +27,50 @@ import static com.heaven7.java.data.mediator.FieldFlags.*;
         @Field(propName = "apiVersion", flags = FLAGS_ALL_SCOPES & ~FLAG_RESET),
         @Field(propName = "net", flags = FLAGS_ALL_SCOPES & ~FLAG_RESET),
 },maxPoolCount = 5)
-public interface AnalysisData extends ICopyable, IResetable{
+public interface AnalysisData extends ICopyable, IResetable, DataPools.Poolable {
+    Property PROP_occurTime = SharedProperties.get("long", "occurTime", 0);
+    Property PROP_enterTime = SharedProperties.get("long", "enterTime", 0);
+    Property PROP_exitTime = SharedProperties.get("long", "exitTime", 0);
+    Property PROP_eventType = SharedProperties.get("java.lang.String", "eventType", 0);
+    Property PROP_tabIndex = SharedProperties.get("int", "tabIndex", 0);
+    Property PROP_itemIndex = SharedProperties.get("int", "itemIndex", 0);
+    Property PROP_item = SharedProperties.get("com.heaven7.data.mediator.demo.module.FlowItem", "item", 0);
+    Property PROP_apiVersion = SharedProperties.get("java.lang.String", "apiVersion", 0);
+    Property PROP_net = SharedProperties.get("java.lang.String", "net", 0);
+
+    long getOccurTime();
+
+    AnalysisData setOccurTime(long occurTime1);
+
+    long getEnterTime();
+
+    AnalysisData setEnterTime(long enterTime1);
+
+    long getExitTime();
+
+    AnalysisData setExitTime(long exitTime1);
+
+    String getEventType();
+
+    AnalysisData setEventType(String eventType1);
+
+    int getTabIndex();
+
+    AnalysisData setTabIndex(int tabIndex1);
+
+    int getItemIndex();
+
+    AnalysisData setItemIndex(int itemIndex1);
+
+    FlowItem getItem();
+
+    AnalysisData setItem(FlowItem item1);
+
+    String getApiVersion();
+
+    AnalysisData setApiVersion(String apiVersion1);
+
+    String getNet();
+
+    AnalysisData setNet(String net1);
 }
